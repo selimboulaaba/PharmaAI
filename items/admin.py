fe2f4112ee9c9506bd56d03d095fc17b05d30196
Selim Boulaaba <selimboulaaba00@gmail.com>
2024-10-04 03:07:11 +0100

    marketplace

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Category, Item, Order, OrderItem
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
---
+++ items/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from items.models import Cart, CartItem
+
+
+class AddToCartForm(forms.ModelForm):
+    class Meta:
+        model = CartItem
+        fields = ['quantity', 'item']
+        widgets = {
+            'quantity': forms.NumberInput(attrs={'class': 'form-control col-md-12'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['quantity'].widget.attrs.update({'min': 1})
